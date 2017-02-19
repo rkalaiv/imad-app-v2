3061e76ceaa0f20527fbfd10cef75c6f25225e8d
rkalaiv <softlinne.kv@gmail.com>
2017-02-19 19:01:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="changed main content";
+var img=document.getElementById("madi");
+img.onclick = function() {
+    img.style.marginleft="100px";
+    };
+
+    
