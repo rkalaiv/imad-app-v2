bc857d98041961877adfec7f37aeb8f9278b9312
kalai <softlinne.kv@gmail.com>
2017-02-27 15:23:09 +0530

    image move success

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,14 @@ console.log('Loaded!');
 var img=document.getElementById("kk");
 
 var marginleft = 0;
-function Setinterval(){
+function moveright(){
     marginleft=marginleft+10;
-    img.style.marginleft= marginleft + "px";
+    img.style.marginLeft= marginleft + "px";
 }
 
 img.onclick = function() {
     //img.style.marginleft="100 px";
-    var interval = setinterval(moveright, 100);
+    var interval = setInterval(moveright, 100);
     };
 
     var element=document.getElementById("main-text");
