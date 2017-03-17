63ab97383938b6b4b981776b6697ed9cde4e5155
rkalaiv <softlinne.kv@gmail.com>
2017-03-17 14:37:00 +0530

    api work finished

--- server.js
+++ server.js
@@ -95,6 +95,17 @@ counter=counter+1;
 res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name', function (req, res) {
+
+var name=req.query.name;
+names.push(name);
+res.send(JSON.stringify(names));
+
+
+});
+
+
 
 app.get('/:articlename', function (req, res) {
     var articlename=req.params.articlename;
@@ -113,6 +124,11 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+app.get('/ui/doll.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'doll.png'));
+});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 
 
 var button=document.getElementById("counter");
-;
+
 
 button.onclick = function() {
 
@@ -23,5 +23,39 @@ span.innerHTML=counter.toString();
 
 request.open("GET","http://localhost:8080/counter",true);
 request.send(null);
+
+};
+
+var submit=document.getElementById("submit_btn");
+
+
+submit.onclick  = function() {
+
+var request=new XMLHttpRequest();
+
+request.onreadystatechange=function(){
+if(request.readyState==XMLHttpRequest.DONE){
+if(request.status==200){
+
+var names=request.responseText;
+names=JSON.parse(names);
+var list="";
+for(var i=0;i<names.length;i++){
+list+="<li>" + names[i] + "</li>";
+}
+var ul=document.getElementById("namelist");
+ul.innerHTML=list;
+
+}
+}
+
+};
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+nameInput.value="";
+
+request.open("GET","http://localhost:8080/submit-name?name="+name,true);
+request.send(null);
+
 
 };
