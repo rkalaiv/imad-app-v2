ee508375e29e8e97d7de40b724c2eb07f63dfce1
rkalaiv <softlinne.kv@gmail.com>
2017-03-18 17:00:18 +0530

    lot changes in server

--- server.js
+++ server.js
@@ -75,6 +75,46 @@ app.get('/hash/:input',function(req,res){
 
 });
 
+app.post('/login',function(req,res){
+
+var username=req.body.username;
+var password=req.body.password;
+
+pool.query("SELECT * FROM 'user' where username=$1",[username],function(err,result){
+
+if(err){
+res.status(500).send(err.toString());
+}else
+{
+
+	if (result.rows.length==0){
+		res.send(403).send("username/password is invalid");
+	}else{
+
+	var dbstring=result.rows(0).password;
+	var salt=dbstring.split('$')[2];
+	var hashedpassword=hash(password,salt);
+	if (hashedpassword==dbstring){
+		res.send("credentials are correct");
+
+	
+	}else{
+
+		res.send(403).send("username/password is invalid");
+	}
+	
+	}
+
+}
+
+
+
+
+});
+
+
+});
+
 
 app.post('/create-user',function(req,res){
 
